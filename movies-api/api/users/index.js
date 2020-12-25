b6bf08344cd24d947e72eb0e7c77df28793b4d07
Ruichen Pan <3196968196@qq.comm>
2020-12-25 00:19:53 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -7,7 +7,12 @@ const router = express.Router(); // eslint-disable-line
 router.get('/', (req, res) => {
     User.find().then(users =>  res.status(200).json(users));
 });
-
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
 // register
 router.post('/', (req, res ,next) => {
     User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
@@ -23,4 +28,20 @@ router.put('/:id',  (req, res) => {
     })
     .then(user => res.json(200, user));
 });
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
 export default router;
